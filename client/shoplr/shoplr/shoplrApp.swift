0082f33694c20fed2847335097d1ec1224dfbd6e
Oliver <dev@theoliver.ch>
2020-11-16 19:27:32 +0100

    init xcode project

---
+++ client/shoplr/shoplr/shoplrApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoplrApp.swift
+//  shoplr
+//
+//  Created by Oliver on 16.11.20.
+//
+
+import SwiftUI
+
+@main
+struct shoplrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
