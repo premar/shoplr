b6efed0a1da5cb69caf97a1e0865cb2e7d8c0c7f
Marius Preuss <premar_@nexit.ch>
2020-11-22 14:45:01 +0100

    fix json bug in server

--- server/shoplr/src/app.py
+++ server/shoplr/src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-import json
 
 application = Flask(__name__, instance_relative_config=True)
 application.config.from_pyfile('config.py')
@@ -14,23 +13,24 @@ def default_route():
 @application.route('/v1/list/', methods=['POST'])
 def new_list():
     if request.method == 'POST':
-        _json = request.json
-        shoplr_list = json.loads(_json)
+        shoplr_list = request.json
         if shoplr_list['key'] == application.config['API_KEY']:
 
-            data = {'identifier': 'meta', 'name': shoplr_list['name'], 'icon': shoplr_list['icon']}
-            json_data = json.dumps(data)
+            data = {
+                'identifier': 'meta',
+                'name': shoplr_list['name'],
+                'icon': shoplr_list['icon']}
 
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[shoplr_list["uuid"]]
-            collection.insert_one(json_data)
+            collection.insert_one(data)
 
-            resp = "201 Created!"
+            resp = jsonify({'message': '201 Created!'})
             resp.status_code = 201
             return resp
         else:
-            resp = "401 Unauthorized!"
+            resp = jsonify({'message': '401 Unauthorized!'})
             resp.status_code = 401
             return 401
 
@@ -38,8 +38,7 @@ def new_list():
 @application.route('/v1/article/<string:list_uuid>', methods=['POST'])
 def new_article(list_uuid):
     if request.method == 'POST':
-        _json = request.json
-        shoplr_article = json.loads(_json)
+        shoplr_article = request.json
         if shoplr_article['key'] == application.config['API_KEY']:
             data = {'identifier': 'item',
                     'uuid': shoplr_article['uuid'],
@@ -47,72 +46,65 @@ def new_article(list_uuid):
                     'specification': shoplr_article['specification'],
                     'icon': shoplr_article['icon'],
                     'expiryDate': shoplr_article['expiryDate']}
-            json_data = json.dumps(data)
-
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[list_uuid]
-            collection.insert_one(json_data)
-
-            resp = "201 Created!"
+            collection.insert_one(data)
+            resp = jsonify({'message': '201 Created!'})
             resp.status_code = 201
             return resp
         else:
-            resp = "401 Unauthorized!"
+            resp = jsonify({'message': '401 Unauthorized!'})
             resp.status_code = 401
             return resp
 
 
-@application.route('/v1/article/<string:list_uuid>', methods=['GET'])
-def get_article(list_uuid):
+@application.route('/v1/list/<string:list_uuid>/<string:api_key>', methods=['GET'])
+def get_article(list_uuid, api_key):
     if request.method == 'GET':
-        _json = request.json
-        shoplr_validator = json.loads(_json)
-        if shoplr_validator['key'] == application.config['API_KEY']:
+        if api_key == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[list_uuid]
-            return list(collection.find({}))
+
+            resp = jsonify(list(collection.find({}, {'_id': False})))
+            return resp
         else:
-            resp = "401 Unauthorized!"
+            resp = jsonify({'message': '401 Unauthorized!'})
             resp.status_code = 401
             return resp
 
 
-@application.route('/v1/list/<string:list_uuid>', methods=['DELETE'])
-def delete_list(list_uuid):
+@application.route('/v1/list/<string:list_uuid>/<string:api_key>', methods=['DELETE'])
+def delete_list(list_uuid, api_key):
     if request.method == 'DELETE':
-        _json = request.json
-        shoplr_validator = json.loads(_json)
-        if shoplr_validator['key'] == application.config['API_KEY']:
+        if api_key == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[list_uuid]
             collection.drop()
-            resp = "200 Deleted!"
-            resp.status_code = 200
+            resp = jsonify({'message': '204 Deleted!'})
+            resp.status_code = 204
             return resp
         else:
-            resp = "401 Unauthorized!"
+            resp = jsonify({'message': '401 Unauthorized!'})
             resp.status_code = 401
             return resp
 
 
-@application.route('/v1/article/<string:list_uuid>/<string:article_uuid>', methods=['DELETE'])
-def delete_item(list_uuid, article_uuid):
+@application.route('/v1/article/<string:list_uuid>/<string:article_uuid>/<string:api_key>', methods=['DELETE'])
+def delete_item(list_uuid, article_uuid, api_key):
     if request.method == 'DELETE':
-        _json = request.json
-        shoplr_validator = json.loads(_json)
-        if shoplr_validator['key'] == application.config['API_KEY']:
+        if api_key == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[list_uuid]
             collection.remove({'uuid': article_uuid})
-            resp = "200 Deleted!"
+            resp = jsonify({'message': '204 Deleted!'})
             resp.status_code = 200
             return resp
         else:
-            resp = "401 Unauthorized!"
+            resp = jsonify({'message': '401 Unauthorized!'})
             resp.status_code = 401
             return resp
 
