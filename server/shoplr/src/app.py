c693797ae002e9f61b6eceead98a641358122550
Marius Preuss <premar_@nexit.ch>
2020-11-30 14:58:28 +0100

    :hammer: refactor server uuid to id, list and item can now be dynamically changed

--- server/shoplr/src/app.py
+++ server/shoplr/src/app.py
@@ -15,14 +15,12 @@ def new_list():
     if request.method == 'POST':
         shoplr_list = request.json
         if request.headers['Authorization'] == application.config['API_KEY']:
-            data = {
-                'identifier': 'meta',
-                'name': shoplr_list['name'],
-                'icon': shoplr_list['icon']}
+            data = shoplr_list
+            data['identifier'] = 'list'
 
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
-            collection = db[shoplr_list["uuid"]]
+            collection = db[shoplr_list["id"]]
             collection.insert_one(data)
 
             resp = jsonify({'message': '201 Created!'})
@@ -34,22 +32,16 @@ def new_list():
             return 401
 
 
-@application.route('/v1/item/<string:list_uuid>/', methods=['POST'])
-def new_item(list_uuid):
+@application.route('/v1/item/<string:list_id>/', methods=['POST'])
+def new_item(list_id):
     if request.method == 'POST':
         shoplr_item = request.json
         if request.headers['Authorization'] == application.config['API_KEY']:
-            data = {'identifier': 'item',
-                    'id': shoplr_item['id'],
-                    'name': shoplr_item['name'],
-                    'specification': shoplr_item['specification'],
-                    'icon': shoplr_item['icon'],
-                    'expiryDate': shoplr_item['expiryDate'],
-                    'bought': shoplr_item['bought']
-                    }
+            data = shoplr_item
+            data['identifier'] = 'item'
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
-            collection = db[list_uuid]
+            collection = db[list_id]
             collection.insert_one(data)
             resp = jsonify({'message': '201 Created!'})
             resp.status_code = 201
@@ -60,22 +52,21 @@ def new_item(list_uuid):
             return resp
 
 
-@application.route('/v1/list/<string:list_uuid>/', methods=['GET'])
-def get_item(list_uuid):
+@application.route('/v1/list/<string:list_id>/', methods=['GET'])
+def get_item(list_id):
     if request.method == 'GET':
         if request.headers['Authorization'] == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
-            collection = db[list_uuid]
+            collection = db[list_id]
             elements = list(collection.find({}, {'_id': False}))
 
             shopping_list = {}
             items = []
 
             for element in elements:
-                if element["identifier"] == "meta":
+                if element["identifier"] == "list":
                     shopping_list = dict(element)
-                    shopping_list['id'] = list_uuid
                     del shopping_list["identifier"]
                 if element["identifier"] == "item":
                     item = dict(element)
@@ -91,13 +82,13 @@ def get_item(list_uuid):
             return resp
 
 
-@application.route('/v1/list/<string:list_uuid>/', methods=['DELETE'])
-def delete_list(list_uuid):
+@application.route('/v1/list/<string:list_id>/', methods=['DELETE'])
+def delete_list(list_id):
     if request.method == 'DELETE':
         if request.headers['Authorization'] == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
-            collection = db[list_uuid]
+            collection = db[list_id]
             collection.drop()
             resp = jsonify({'message': '204 Deleted!'})
             resp.status_code = 204
@@ -108,14 +99,14 @@ def delete_list(list_uuid):
             return resp
 
 
-@application.route('/v1/item/<string:list_uuid>/<string:item_uuid>/', methods=['DELETE'])
-def delete_item(list_uuid, item_uuid):
+@application.route('/v1/item/<string:list_id>/<string:item_id>/', methods=['DELETE'])
+def delete_item(list_id, item_id):
     if request.method == 'DELETE':
         if request.headers['Authorization'] == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
-            collection = db[list_uuid]
-            collection.remove({'uuid': item_uuid})
+            collection = db[list_id]
+            collection.remove({'id': item_id})
             resp = jsonify({'message': '204 Deleted!'})
             resp.status_code = 204
             return resp
