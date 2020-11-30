fa090a5a74bd1bb941e78dba1ba446d1ebd7c46f
Marius Preuss <premar_@nexit.ch>
2020-11-30 13:01:04 +0100

    change function to return list

--- server/shoplr/src/app.py
+++ server/shoplr/src/app.py
@@ -34,17 +34,19 @@ def new_list():
             return 401
 
 
-@application.route('/v1/item/<string:list_uuid>', methods=['POST'])
+@application.route('/v1/item/<string:list_uuid>/', methods=['POST'])
 def new_item(list_uuid):
     if request.method == 'POST':
         shoplr_item = request.json
         if request.headers['Authorization'] == application.config['API_KEY']:
             data = {'identifier': 'item',
-                    'uuid': shoplr_item['uuid'],
+                    'id': shoplr_item['id'],
                     'name': shoplr_item['name'],
                     'specification': shoplr_item['specification'],
                     'icon': shoplr_item['icon'],
-                    'expiryDate': shoplr_item['expiryDate']}
+                    'expiryDate': shoplr_item['expiryDate'],
+                    'bought': shoplr_item['bought']
+                    }
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[list_uuid]
@@ -58,15 +60,30 @@ def new_item(list_uuid):
             return resp
 
 
-@application.route('/v1/list/<string:list_uuid>', methods=['GET'])
+@application.route('/v1/list/<string:list_uuid>/', methods=['GET'])
 def get_item(list_uuid):
     if request.method == 'GET':
         if request.headers['Authorization'] == application.config['API_KEY']:
             client = MongoClient(application.config["DB_SERVER_URI"])
             db = client[application.config["DB_NAME"]]
             collection = db[list_uuid]
+            elements = list(collection.find({}, {'_id': False}))
 
-            resp = jsonify(list(collection.find({}, {'_id': False})))
+            shopping_list = {}
+            items = []
+
+            for element in elements:
+                if element["identifier"] == "meta":
+                    shopping_list = dict(element)
+                    shopping_list['id'] = list_uuid
+                    del shopping_list["identifier"]
+                if element["identifier"] == "item":
+                    item = dict(element)
+                    del item["identifier"]
+                    items.append(item)
+            shopping_list['items'] = items
+            resp = jsonify(shopping_list)
+            resp.status_code = 200
             return resp
         else:
             resp = jsonify({'message': '401 Unauthorized!'})
@@ -74,7 +91,7 @@ def get_item(list_uuid):
             return resp
 
 
-@application.route('/v1/list/<string:list_uuid>', methods=['DELETE'])
+@application.route('/v1/list/<string:list_uuid>/', methods=['DELETE'])
 def delete_list(list_uuid):
     if request.method == 'DELETE':
         if request.headers['Authorization'] == application.config['API_KEY']:
@@ -100,7 +117,7 @@ def delete_item(list_uuid, item_uuid):
             collection = db[list_uuid]
             collection.remove({'uuid': item_uuid})
             resp = jsonify({'message': '204 Deleted!'})
-            resp.status_code = 200
+            resp.status_code = 204
             return resp
         else:
             resp = jsonify({'message': '401 Unauthorized!'})
